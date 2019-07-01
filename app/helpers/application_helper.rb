7f0808e53380360bda5fdddbe2896bd7b1b53e18
Minh Khoi <ho.minh.khoi@sun-asterisk.com>
2019-07-01 15:32:52 +0700

    Chapter 4:  Rails-flavored Ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def full_title page_title = "", base_title = ""
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
 end
